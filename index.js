24a47d747ca96c62cf6df77f5a689d44f18bc283
“shazaib491” <shazaibrehman127@gmail.com>
2022-05-15 20:30:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// console.log("Helo wolrd");
+// var a=10;
+// console.log(a);
+
+// // float double boolean
+
+// // byte short int long  === combination + float double 
+// // js ==>  Number
+
+// // /character string 
+// // string
+
+// // boolean
+// // true false
+
+// // undefined 
+
+// var a=undefined;
+
+// var a=10.2;
+// var b=20;
+// var c=a+b;
+// console.log(c)
+// var name="Sonali"
+
+// console.log(name)
+// var flag=true;
+// console.log(flag);
+// var username;
+// console.log(username);
+
+
+// artmatic operator
+// +
+// -
+// *
+// /
+// ** exponention 
+// ++
+// --
+// 5  5
+
+
+// var j=5;
+// var k=5;
+// k++; //6
+// // j=j-1;
+// j--;
+// // var f=k**j;
+// console.log("decrementation",j);
+// modulas operator
+
+// 2 % 2 ==0
+//  2 % 3 == 1
+
+
+var num=10;
+// num=num+10;
+num=5;
+console.log(num);
+
+var fname="10"
+var sname=20
+
+var username=fname+sname;
+console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
